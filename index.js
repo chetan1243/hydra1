e9d8dc59d5a755ecbd492d559fee60fadb28f8ed
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2019-01-21 18:05:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const applyText = (canvas, text) => {
 };
 
 client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find(ch => ch.name === '📡welcome-to-hub📡');
+	const channel = member.guild.channels.find(ch => ch.name === '✨welcome-boizzz-✨');
 	if (!channel) return;
 
 	const canvas = Canvas.createCanvas(700, 250);
